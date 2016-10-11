a007347bb0aa3977258966558011f100bf25b8e2
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-10-11 17:11:06 -0500

    sphere has movement

---
+++ Tutorials/Agent Behavior/Assets/Scripts/Behavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Behavior : MonoBehaviour
+{
+    //Force = mass * velocity
+    //velocity = Force / mass
+
+    public Vector3 force;
+    public int mass;
+    Vector3 velocity;
+
+    void FixedUpdate()
+    {
+        velocity = ((force / mass) * Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+        transform.position += velocity;
+    }
+}
