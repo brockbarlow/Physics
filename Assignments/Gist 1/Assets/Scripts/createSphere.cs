f20e7046947ca62915d7cb9e5492fa2ba6b22db6
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-10-11 10:07:44 -0500

    created vertex struct

---
+++ Assignments/Gist 1/Assets/Scripts/createSphere.cs
@@ -0,0 +1,23 @@
+﻿/*gist1: refactor your c++ generate halfcircle and generate sphere functions to c# then execute them in unityengine*/
+
+using UnityEngine;
+using System.Collections;
+
+struct Vertex
+{
+    Vector4 position;
+    Vector4 color;
+}
+
+public class createSphere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
