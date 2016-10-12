76412b6605adc77eb9d892688649b9195c4de2a2
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-10-12 11:18:11 -0500

    worked on calculations

--- Tutorials/Agent Behavior/Assets/Scripts/Behavior.cs
+++ Tutorials/Agent Behavior/Assets/Scripts/Behavior.cs
@@ -7,13 +7,34 @@ public class Behavior : MonoBehaviour
     //Force = mass * velocity
     //velocity = Force / mass
 
-    public Vector3 force;
-    public int mass;
+    Vector3 force;
+    float mass;
     Vector3 velocity;
+
+    Vector3 steering;
+    Vector3 desiredVelocity;
+    Vector3 displacement;
+    public Transform target;
+
+    void Start()
+    {
+        force.x = Random.Range(10, 75);
+        force.y = Random.Range(10, 75);
+        force.z = Random.Range(10, 75);
+        mass = Random.Range(10, 75);
+    }
 
     void FixedUpdate()
     {
-        velocity = ((force / mass) * Time.deltaTime);
+        displacement = target.position - transform.position;
+        desiredVelocity = displacement.normalized;
+        steering = (desiredVelocity - velocity).normalized / mass;
+        velocity += steering;
+
+        if (velocity.magnitude > 5)
+        {
+            velocity = velocity.normalized;
+        }
     }
 
     void LateUpdate()
