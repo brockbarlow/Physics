c6143ab84a40f7890a52bfbd37bfc5066f38ce00
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-10-12 13:06:12 -0500

    work before lecture

--- Tutorials/Agent Behavior/Assets/Scripts/Agent.cs
+++ Tutorials/Agent Behavior/Assets/Scripts/Agent.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Agent : MonoBehaviour {
+    Vector3 velocity;
+    Vector3 force;
+    float mass;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    Vector3 displacement;
+    Vector3 desiredVelocity;
+    Vector3 steering;
+    public Transform target;
+
+    void FixedUpdate()
+    {
+        displacement = target.position - transform.position;
+        desiredVelocity = displacement.normalized;
+        steering = (desiredVelocity - velocity).normalized / mass;
+        velocity += steering;
+
+        if (velocity.magnitude > 5)
+        {
+            velocity = velocity.normalized;
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.position += velocity;
+    }
 }
