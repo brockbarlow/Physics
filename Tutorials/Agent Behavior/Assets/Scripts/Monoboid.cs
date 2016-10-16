edf570cb62ceaaffe0af15c0d8364287f16cf936
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-10-15 21:15:04 -0500

    created monoboid script and edited seek script

---
+++ Tutorials/Agent Behavior/Assets/Scripts/Monoboid.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Interface;
+
+public class Monoboid : MonoBehaviour, Iboid
+{
+    private Agent a;
+    public float mass;
+
+    public Vector3 velocity
+    {
+        get { return a.velocity; }
+        set { a.velocity = value; }
+    }
+
+    public Vector3 position
+    {
+        get { return a.position; }
+        set { a.position = value; }
+    }
+
+    float Iboid.mass
+    {
+        get { return a.mass; }
+        set { a.mass = value; }
+    }
+
+    void Start()
+    {
+        a = new Agent(mass);
+    }
+
+    void LateUpdate()
+    {
+        a.updateVelocity();
+    }
+}
---
+++ Tutorials/Agent Behavior/Assets/Scripts/Seek.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seek : MonoBehaviour
+{
+    Vector3 desiredVelocity;
+    Vector3 steering;
+    public Transform target;
+    public float steer;
+    Monoboid agent;
+
+    void Awake()
+    {
+        agent = gameObject.GetComponent<Monoboid>();
+    }
+
+    void FixedUpdate()
+    {
+        desiredVelocity = (target.position - transform.position).normalized;
+        steering = (desiredVelocity - agent.velocity).normalized * steer;
+        agent.velocity += steering / agent.mass;
+        if (agent.velocity.magnitude > 5)
+        {
+            agent.velocity = agent.velocity.normalized;
+        }
+    }
+}
