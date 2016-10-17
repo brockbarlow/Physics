4759f8cd914b04e6e80fba4d2f194db7cf866881
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-10-17 09:28:25 -0500

    created new cs items

---
+++ Assignments/Gist 2/Gist 2/Interface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gist_2
+{
+    interface Interface
+    {
+    }
+}
--- Assignments/Gist 2/Gist 2/Program.cs
+++ Assignments/Gist 2/Gist 2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿/*Gist: begin designing a finite state machine in c#. All states must implenent interfaces.*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
