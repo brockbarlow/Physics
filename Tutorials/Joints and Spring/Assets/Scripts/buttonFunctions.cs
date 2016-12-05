9f1ce3d06aebff2e0770e5628013dc9ba9df8a14
Brock Barlow <brock.barlow@students.aie.edu.au>
2016-12-05 17:51:16 -0600

    added controls button

--- Tutorials/Joints and Spring/Assets/Scripts/buttonFunctions.cs
+++ Tutorials/Joints and Spring/Assets/Scripts/buttonFunctions.cs
@@ -7,10 +7,19 @@
     {   //button function. when the user "clicks" the button labeled "refresh", the
         //cloth simulation scene will be reloaded. this button should be used to
         //rebuild the cloth if 1)cloth is broken or 2)show different outcomes of the
-        //project.
+        //project. 
+
+        //When the user clicks the button labeled "Controls", the app will
+        //load the controls scene. controls scene informs the user what to use
+        //during run time.
         public void Restart()
         {
             SceneManager.LoadScene("Main");
+        }
+
+        public void Controls()
+        {
+            SceneManager.LoadScene("Controls");
         }
     }
 }
